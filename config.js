4c578e16293020762b330a9874c014028bf43b20
𝐂ʀᴏɴᴀᴢ <waxx3832@gmail.com>
2025-01-11 09:00:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  PREFIX: process.env.PREFIX || "IRON-M4N×",
-  EMAIL: process.env.EMAIL || "ironman@onlyfans.com",
-  PASS: process.env.PASS || "Kimi da yo kimi nandayo..",
-  MESSAGE: process.env.MESSAGE || "Thank you for using our bot dont share your session id blah blah .etc",
-  IMAGE: process.env.IMAGE || "https://cdn.ironman.my.id/i/2iceb4.jpeg",
+  PREFIX: process.env.PREFIX || "",
+  EMAIL: process.env.EMAIL || "klwhiteser@gmail.com",
+  PASS: process.env.PASS || "KL WHITE SER@123",
+  MESSAGE: process.env.MESSAGE || "Thank you for using ACD MD BOT V2 dont share your session id",
+  IMAGE: process.env.IMAGE || "https://cdn.ironman.my.id/i/2iceb4.jpeg\n\nhttps://whatsapp.com/channel/0029VawxBmHFnSzBLlIQ3y1e",
 }
