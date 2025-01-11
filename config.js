39c9f5223d2e98922090995b228e3a35df0f73a9
𝐂ʀᴏɴᴀᴢ <waxx3832@gmail.com>
2025-01-11 09:23:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ module.exports = {
   EMAIL: process.env.EMAIL || "klwhiteser@gmail.com",
   PASS: process.env.PASS || "KL WHITE SER@123",
   MESSAGE: process.env.MESSAGE || "Thank you for using ACD MD BOT V2 dont share your session id",
-  IMAGE: process.env.IMAGE || "https://cdn.ironman.my.id/i/2iceb4.jpeg\n\nhttps://whatsapp.com/channel/0029VawxBmHFnSzBLlIQ3y1e",
+  IMAGE: process.env.IMAGE || "https://cdn.ironman.my.id/i/2iceb4.jpeg",
 }
